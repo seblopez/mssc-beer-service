c9e795e0a8a122c67fb389f7335399c24d47a202
sebastianlopez <sebalopez2311@gmail.com>
2020-08-08 17:52:53 -0300

    Added AllocationOrder event

--- src/main/java/guru/springframework/msscbeerservice/event/NewInventoryEvent.java
+++ src/main/java/guru/springframework/msscbeerservice/event/NewInventoryEvent.java
@@ -2,7 +2,7 @@ package guru.springframework.msscbeerservice.event;
 
 import guru.springframework.msscbeerservice.web.model.BeerDto;
 
-public class NewInventoryEvent extends BeerEvent{
+public class NewInventoryEvent extends BeerEvent {
 
     public NewInventoryEvent(BeerDto beerDto) {
         super(beerDto);
